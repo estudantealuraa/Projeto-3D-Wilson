eca16afa57c0f7a33f8fa1bb81f871bf9e66b11f
Ana J.C.M <105929858+estudantealuraa@users.noreply.github.com>
2024-03-12 10:30:45 -0300

    fdssd

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const botoes = document.querySelectorAll(".botao");
 
 for (let i = 0; i < botoes.length; i++){
     botoes[i].onlinck = function (){
+        botoes[i].classList.add("ativo")
         for (let j = 0; j < botoes.length; j++){
             botoes[j].classList.remove("ativo");
         }
