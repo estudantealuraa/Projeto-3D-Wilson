d69d97a6b4686d6dfedd1e7d3cedd685a3eb4c5e
Ana J.C.M <105929858+estudantealuraa@users.noreply.github.com>
2024-03-12 10:24:31 -0300

    twe

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ const botoes = document.querySelectorAll(".botao");
 
 for(let i = 0; i< botoes.length;i++){
     botoes[i].onclick = function(){
+        botoes[i].classlist.add("ativo");
         for(letj = 0; j< botoes.length; j++){
-            botoes[i].classlist.remove("ativo");
+            botoes[j].classlist.remove("ativo");
         }
         botoes[i].classList.add("ativo")
     }
