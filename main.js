6100063f6798af37e11555c1131f057627a12423
Ana J.C.M <105929858+estudantealuraa@users.noreply.github.com>
2024-03-12 10:24:02 -0300

    few

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const botoes = document.querySelectorAll(".botao");
 
 for(let i = 0; i< botoes.length;i++){
     botoes[i].onclick = function(){
-        botoes[i].classlist.add("ativo");
         for(letj = 0; j< botoes.length; j++){
             botoes[i].classlist.remove("ativo");
         }
