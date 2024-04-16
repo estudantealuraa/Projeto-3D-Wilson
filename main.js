d38d09a1697d25fc9664bd65a1012b1267cf3d56
Ana J.C.M <105929858+estudantealuraa@users.noreply.github.com>
2024-04-16 10:26:16 -0300

    cnbn

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ for (let i = 0; i < botoes.length; i++){
     }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025/01/01");
+const tempoObjetivo1 = new Date("2025-01-01");
 let tempoAtual = new Date();
 
 contadores[0].textContent = twmpoObjetivo1 - tempoAtual;
