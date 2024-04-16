9fd44a4eee0f8a889f86c6bb1207c938f5b826a9
Ana J.C.M <105929858+estudantealuraa@users.noreply.github.com>
2024-04-16 10:59:32 -0300

    fdfd

--- main.js
+++ main.js
@@ -18,3 +18,5 @@ let segundos = Math.floor(tempoFinal/ 1000);
 let minutos = Math.floor(segundos/ 60);
 let horas = Math.floor(minutos/60);
 let dias = Math.floor(horas/24);
+
+return dias + "dias" + horas "horas" + minutos + "minutos" + segundos + "segundos";
