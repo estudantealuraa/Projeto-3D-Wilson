9807773531f2c86fd7fd9e192f0c5e2150975924
Ana J.C.M <105929858+estudantealuraa@users.noreply.github.com>
2024-04-30 10:54:16 -0300

    hfhf

--- main.js
+++ main.js
@@ -24,6 +24,7 @@ contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 
 function calculaTempo(tempoObjetivo1){
     let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo1 - tempoAtual;
     let segundos = Math.floor(tempoFinal / 1000);
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
