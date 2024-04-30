ea0c8b31b833a64842821f5050046ef530ba5a88
Ana J.C.M <105929858+estudantealuraa@users.noreply.github.com>
2024-04-30 10:28:20 -0300

    bdfgf

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo")
 
 for (let i = 0; i<botoes.length; i++){
-    botoes[i].onclick = function (){
+    botoes[i].onclick = function(){
         for (let j = 0; j < botoes.length; j++){
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
@@ -13,6 +13,9 @@ for (let i = 0; i<botoes.length; i++){
 }
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-01-01");
+const tempoObjetivo2 = new Date("2025-01-01");
+const tempoObjetivo3 = new Date("2025-01-01");
+const tempoObjetivo4 = new Date("2024-12-14");
 let tempoAtual = new Date();
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 let segundos = Math.floor(tempoFinal / 1000);
